dda83ed9f27b95294f66d80c64c9414154204171
spbryan <seanbryan@comcast.com>
2019-08-24 13:03:48 -0400

    pluralize status on routes and fix validation issue with field length

--- routes/api/status.js
+++ routes/api/status.js
@@ -9,12 +9,12 @@
 const router = require("express").Router();
 const statusController = require("../../controllers/statusController");
 
-// Matches with "/api/status"
+// Matches with "/api/statuses"
 router.route("/")
   .get(statusController.findAll)
   .post(statusController.create);
 
-// Matches with "/api/status/:id"
+// Matches with "/api/statuses/:id"
 router
   .route("/:id")
   .get(statusController.findById)
