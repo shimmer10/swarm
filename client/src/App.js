122df2c4cce270be4ecf868c182ac942b52bd76b
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-08-24 12:44:05 -0400

    adding custom toggle components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Hometwo from "./pages/Hometwo";
 import NavBar from "./components/Nav";
 import NoMatch from "./pages/NoMatch";
+import Main from "./pages/Main";
 import LoginScreen from "./pages/LoginScreen";
 import "./App.css";
 
@@ -13,14 +14,15 @@ function App() {
       <div>
         <NavBar />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Hometwo} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/homeTwo" component={Hometwo} />
           <Route exact path="/loginscreen" component={LoginScreen} />
           <Route component={NoMatch} />
         </Switch>
       </div>
     </Router>
   );
-}  
+}
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,55 +1,39 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import Footer from '../components/Footer';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
-import './Home.css';
+import DatePicker from 'react-date-picker';
+import Dropdown from 'react-bootstrap/Dropdown';
+import CustomToggle from '../components/CustomToggle';
+import CustomMenu from '../components/CustomMenu';
 
 class Home extends Component {
+    state = {
+        date: new Date(),
+    }
+
+    onChange = date => this.setState({ date })
 
     render() {
         return (
             <div>
-                <Container id="container">
-                    <Row>
-                        <Col size="md-12">
-                            <Image id="main-logo" src={require('../images/SWARMLogo.png')} alt="SWARM Logo" fluid />
-                        </Col>
-                    </Row>
-                    <Row fluid>
-                        <Col size="md-12">
-                            <h1>Welcome To SWARM</h1>
-                            <h6>Standup With A Remote Motif</h6>
-                        </Col>
-                    </Row>
-                    <Row fluid>
-                        <Col size="md-2">
+                <Dropdown>
+                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
+                        Custom toggle
+                    </Dropdown.Toggle>
 
-                        </Col>
-                        <Col size="md-8" id="home-text">
-
-                            <div id="main-section">
-                                <br />
-                                <h4>Our goal is to help relieve the struggles of working with members from long
-                                    distances while still following agile practices.
-                            </h4>
-                                <h4>We want to help solve the remote daily standup conundrum!</h4>
-                                <h4>
-                                    <Link className="ml-auto" id="login" to="/loginscreen">Sign up </Link>
-                                    today to start having improved standups!
-                            </h4>
-                            </div>
-                        </Col>
-                        <Col size="md-2">
-
-                        </Col>
-                    </Row>
-                </Container>
-                <Footer />
+                    <Dropdown.Menu as={CustomMenu}>
+                        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+                        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+                        <Dropdown.Item eventKey="3" active>
+                            Orange
+                        </Dropdown.Item>
+                        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <DatePicker
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
             </div>
-        );
+        )
     }
 }
 
