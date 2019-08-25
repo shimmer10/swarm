9445530805d488403d2b01a6cdeb16b9bb847d79
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-08-25 18:09:29 -0400

    redirect works to send to session page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 import API from "../utils/API";
 import Button from 'react-bootstrap/Button';
 import CustomToggle from '../components/CustomToggle';
@@ -9,10 +10,11 @@ import Dropdown from 'react-bootstrap/Dropdown';
 class Home extends Component {
     state = {
         date: new Date(),
-        teams: []
+        teams: [],
+        redirect: false
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.getTeams();
     }
 
@@ -20,22 +22,34 @@ class Home extends Component {
 
     getTeams = () => {
         API.getTeams()
-          .then(res =>
-            this.setState({
-              teams: res.data
-            })
-          )
-          .catch(() =>
-            this.setState({
-              teams: []
-            })
-          );
-      };
+            .then(res =>
+                this.setState({
+                    teams: res.data
+                })
+            )
+            .catch(() =>
+                this.setState({
+                    teams: []
+                })
+            );
+    };
+
+      setRedirect = () => {
+        this.setState({
+          redirect: true
+        })
+      }
+      renderRedirect = () => {
+        if (this.state.redirect) {
+          return <Redirect to='/session' />
+        }
+      }
 
     render() {
         console.log("teams: " + JSON.stringify(this.state.teams));
         return (
             <div>
+                {this.renderRedirect()}
                 <Dropdown>
                     <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
                         Choose Team
@@ -51,7 +65,8 @@ class Home extends Component {
                     onChange={this.onChange}
                     value={this.state.date}
                 />
-                <Button variant="outline-primary">Submit</Button>
+                <Button variant="outline-primary" size="lg" className="px-4"
+                    onClick={this.setRedirect}>Submit</Button>
             </div>
         )
     }
